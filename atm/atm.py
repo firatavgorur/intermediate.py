8ddf8d582077144e83d941c00dc7b1000227eaf6
Fırat Avgörür <89043343+firatavgorur@users.noreply.github.com>
2021-11-01 19:35:02 +0300

    Update atm.py

--- atm/atm.py
+++ atm/atm.py
@@ -7,64 +7,61 @@ FıratHesap = {
 }
 
 def paraCek():
-    tutar = input("Çekmek İstediğiniz Tutarı Giriniz: ")
+    tutar = input("\nÇekmek İstediğiniz Tutarı Giriniz: ")
     if int(tutar) > FıratHesap["bakiye"]:
         bakiyeSorgula(FıratHesap)
-        soru2 = input("Bakiyeniz yetersizdir. Ek hesap kullanmak ister misiniz?: ")
+        soru2 = input("\nBakiyeniz yetersizdir. Ek hesap kullanmak ister misiniz?: ")
         if soru2 == "Evet":
-            soru3 = input("Çekmek istediğiniz tutarı giriniz? :")
+            soru3 = input("\nÇekmek istediğiniz tutarı giriniz? :")
             if int(soru3) > (FıratHesap["bakiye"] + FıratHesap["ekHesap"]):
-                print("Bakiyeniz yetersizdir lütfen geçerli tutar giriniz.")
+                print("\nBakiyeniz yetersizdir lütfen geçerli tutar giriniz.")
             else:
                 if int(soru3) == (FıratHesap["bakiye"] + FıratHesap["ekHesap"]) or int(soru3) < (FıratHesap["bakiye"] + FıratHesap["ekHesap"]):
                     bakiye = FıratHesap["bakiye"]
                     ekhesapkullanıcakmiktar = int(soru3) - FıratHesap["bakiye"]
                     FıratHesap["bakiye"] = 0
                     FıratHesap["ekHesap"] -= ekhesapkullanıcakmiktar
-                    print(f"İstediğiniz tutar hazırlanıyor.Kalan bakiyeniz, Ana Hesap : {FıratHesap['bakiye']} TL, Ek Hesap: {FıratHesap['ekHesap']}" )
+                    print(f"\nİstediğiniz tutar hazırlanıyor.Kalan bakiyeniz, Ana Hesap : {FıratHesap['bakiye']} TL, Ek Hesap: {FıratHesap['ekHesap']}" )
                     bakiyeSorgula(FıratHesap)
         else:
-            print("İşleminiz iptal edilmiştir.")
+            print("\nİşleminiz iptal edilmiştir.")
     else:
         if int(tutar) == FıratHesap["bakiye"] or int(tutar) < FıratHesap["bakiye"]:
             FıratHesap["bakiye"] -= int(tutar)
-            print("Paranız Hazırlanıyor..")
+            print("\nParanız Hazırlanıyor..")
             bakiyeSorgula(FıratHesap)
 
 
 def paraYatir():
-    tutar = input("Yatırmak İstediğiniz Tutarı Giriniz: ")
+    tutar = input("\nYatırmak İstediğiniz Tutarı Giriniz: ")
     if int(tutar) > 0:
         FıratHesap["bakiye"] += int(tutar)
-        print("Paranız ana hesabınıza yatırılıyor.")
-        print(f"Yeni bakiyeniz : {FıratHesap['bakiye']} TL'dir.Ek hesap bakiyeniz {FıratHesap['ekHesap']} TL'dir. Toplam bakiye = {FıratHesap['bakiye'] + FıratHesap['ekHesap']} TL'dir.")
+        print("\nParanız ana hesabınıza yatırılıyor.")
+        print(f"\nYeni bakiyeniz : {FıratHesap['bakiye']} TL'dir.Ek hesap bakiyeniz {FıratHesap['ekHesap']} TL'dir. Toplam bakiye = {FıratHesap['bakiye'] + FıratHesap['ekHesap']} TL'dir.")
     
     else:
         if int(tutar) <= 0:
-            print("Eksik veya yetersiz tutar girdiniz.")
+            print("\nEksik veya yetersiz tutar girdiniz.")
 
 def bakiyeSorgula(FıratHesap):
-    print(f'{FıratHesap["hesapNo"]} nolu ana hesabınızda {FıratHesap["bakiye"]} tl bulunmaktadır. Ek hesabınızda ise {FıratHesap["ekHesap"]} tl bulunmaktadır.')
+    print(f'\n{FıratHesap["hesapNo"]} nolu ana hesabınızda {FıratHesap["bakiye"]} tl bulunmaktadır. Ek hesabınızda ise {FıratHesap["ekHesap"]} tl bulunmaktadır.')
 
 
 def bakiyeSorgulama():
-    print(f"Merhaba {FıratHesap['ad']}, {FıratHesap['hesapNo']} nolu ana hesabınızda toplam {FıratHesap['bakiye']} TL bulunmaktadır.")
+    print(f"\nMerhaba {FıratHesap['ad']}, {FıratHesap['hesapNo']} nolu ana hesabınızda toplam {FıratHesap['bakiye']} TL bulunmaktadır.")
 
 
-print("\n************ ATM *************\n")
 
-islemler = {
+print("\n************** ATM ***************\n")
 
-    "1" : "Para Çekme",
-    "2" : "Para Yatırma",
-    "3" : "Bakiye Sorgulama",
-    "4" : "Çıkış"
-}
+print("----    '1' : Para Çekme       ----")  
+print("----    '2' : Para Yatırma     ----")
+print("----    '3' : Bakiye Sorgulama ----")
+print("----    '4' : Çıkış            ----")
 
-print(islemler)
+print("\n************** ATM ***************\n")
 
-
-islemSec = input("Yapmak istediğiniz işlemi seçiniz: ")
+islemSec = input("\nYapmak istediğiniz işlemi seçiniz: ")
 if islemSec == "1":
     paraCek()
 elif islemSec == "2":
@@ -75,3 +72,4 @@ else:
     if islemSec == "4":
         print("Hoşçakalın!")
         False
+
